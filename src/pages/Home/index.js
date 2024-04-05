12929f7c56d35b8cc57d3e34c5f6e8174e062ab2
David Masip <davidmasip16@gmail.com>
2024-04-05 13:19:33 +0200

    Fix Autofocus boolean

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,40 @@
-import { React, useState } from 'react'
-import { useLocation } from 'wouter'
-import {useGifs} from '../../hooks/useGifs'
-import ListOfGifs from '../../components/ListOfGifs/ListOfGifs'
+import { React, useState } from "react";
+import { useLocation } from "wouter";
+import { useGifs } from "../../hooks/useGifs";
+import ListOfGifs from "../../components/ListOfGifs/ListOfGifs";
 
-const GIFS_LIMIT = 5
+const GIFS_LIMIT = 5;
 
 function Home() {
-    const [keyword, setKeyword] = useState('')
-    const [path, pushLocation] = useLocation()
-    const {loading, gifs} = useGifs({ keyword: 'Rick', GIFS_LIMIT })
+  const [keyword, setKeyword] = useState("");
+  const [path, pushLocation] = useLocation();
+  const { loading, gifs } = useGifs({ keyword: "Rick", GIFS_LIMIT });
 
-    const handleSubmit = evt => {
-        evt.preventDefault()
-        keyword !== '' && pushLocation(`/search/${keyword}`)
-    }
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    keyword !== "" && pushLocation(`/search/${keyword}`);
+  };
 
-    const handleChange = evt => {
-        setKeyword(evt.target.value)
-    }
+  const handleChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
 
-    return (
-        <>
-            <h1 className="App-title">Giffy Store</h1>
-            <form onSubmit={handleSubmit} className="App-form">
-                <input onChange={handleChange} type="text" value={keyword} placeholder="Search gifs" autoFocus="true" />
-            </form>
-            <h4>Examples</h4>
-            {
-                loading 
-                ? <i>Loading...</i> 
-                : <ListOfGifs gifs={gifs} />
-            }   
-        </>
-    );
+  return (
+    <>
+      <h1 className="App-title">Giffy Store</h1>
+      <form onSubmit={handleSubmit} className="App-form">
+        <input
+          onChange={handleChange}
+          type="text"
+          value={keyword}
+          placeholder="Search gifs"
+          autoFocus={true}
+        />
+      </form>
+      <h4>Examples</h4>
+      {loading ? <i>Loading...</i> : <ListOfGifs gifs={gifs} />}
+    </>
+  );
 }
 
-export default Home
+export default Home;
