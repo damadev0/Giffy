c435fb4f8edae479342a639a4be5c3f8a0eb253b
David Masip <davidmasip16@gmail.com>
2024-04-08 17:35:28 +0200

    Shows "No results" text when getting 0 results

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import ListOfGifs from '../../components/ListOfGifs/ListOfGifs'
-import { useGifs } from '../../hooks/useGifs';
-import './index.css'
+import React from "react";
+import ListOfGifs from "../../components/ListOfGifs/ListOfGifs";
+import { useGifs } from "../../hooks/useGifs";
+import "./index.css";
 
-const GIFS_LIMIT = 30
+const GIFS_LIMIT = 30;
 
-function SearchResults( { params } ) {
-    const { keyword } = params
-    const {loading, gifs} = useGifs({ keyword, GIFS_LIMIT })
-    
-    return <div className='Search'>
-        <h1>Results</h1>
-        {
-            loading 
-            ? <i>Loading...</i> 
-            : <ListOfGifs gifs={gifs} />
-        }
-        </div>
+function SearchResults({ params }) {
+  const { keyword } = params;
+  const { loading, gifs } = useGifs({ keyword, GIFS_LIMIT });
+
+  return (
+    <div className="Search">
+      <h1>Results</h1>
+      {loading ? (
+        <i>Loading...</i>
+      ) : gifs.length > 0 ? (
+        <ListOfGifs gifs={gifs} />
+      ) : (
+        <p>There are no results for your search</p>
+      )}
+    </div>
+  );
 }
 
-export default SearchResults
+export default SearchResults;
