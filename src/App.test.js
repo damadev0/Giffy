704e0333e5f9ce26b5a0e8bd984698bc7fa43d53
David Masip <davidmasip16@gmail.com>
2024-04-08 18:03:22 +0200

    Test if title exists

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Renders Title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Giffy Store/i);
+  expect(title).toBeInTheDocument();
 });
